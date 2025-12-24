85b06123005af15ba36f9107c2c00c1a5698039b
SWARANJIT <166933500+RootAxiom@users.noreply.github.com>
2025-12-24 14:22:43 +0530

    Added initial Go files for "Hello, World!" and simple value demonstrations

---
+++ 2_simple_values/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Simple values in Go
+	// Integer
+	var a int = 10
+	fmt.Println(a)
+	// Float
+	var b float64 = 20.5
+	fmt.Println(b)
+	// String
+	var c string = "Golang"
+	fmt.Println(c)
+	// Boolean
+	var d bool = true
+	fmt.Println(d)
+
+	
+}
