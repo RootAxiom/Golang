eb08184bdf901dddb009b4e636b0dff75886bb0a
SWARANJIT <166933500+RootAxiom@users.noreply.github.com>
2025-12-26 19:28:08 +0530

    refactor: Clean up if-else example and enhance admin permission check

--- 6_if-else/ifelse.go
+++ 6_if-else/ifelse.go
@@ -1,13 +1,26 @@
 package main
 
+import "fmt"
+
 func main(){
    // if-else statement
-   a := 21
-   b := 20
+//    a := 21
+//    b := 20
 
-   if a > b {
-       println("a is greater than b")
-   } else {
-       println("b is greater than a")
-   }
+//    if a > b {
+//        println("a is greater than b")
+//    } else {
+//        println("b is greater than a")
+//    }
+// }
+
+var role ="admin"
+var haspermissions = true
+
+if role == "admin" && haspermissions {
+    fmt.Println("User is an admin and he/she can access the system.")
+} else {
+    fmt.Println("User does not have admin permissions.")
+}
+
 }
