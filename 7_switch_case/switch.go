2402b2572f5881da3f33b9b32e81d3e4decdc398
SWARANJIT <166933500+RootAxiom@users.noreply.github.com>
2026-01-16 08:28:44 +0530

    example: Add Go switch case examples.

---
+++ 7_switch_case/switch.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	// "fmt"
+	// "time"
+)
+
+func main() {
+	// simple switch
+
+	//   i := 3
+
+	//   switch i {
+	// 	case 1:
+	// 		println("i is 1")
+	// 	case 2:
+	// 		println("i is 2")
+	// 	case 3:
+	// 		println("i is 3")
+	// 	default:
+	// 		println("i is not 1, 2, or 3")
+	// 	}
+
+	// switch with multiple conditions
+      
+	// fmt.Println(time.Now().Weekday())
+	// fmt.Println("----------------------------------")
+	// fmt.Println("----------------------------------")
+	// switch time.Now().Weekday() {
+	// 	case time.Saturday, time.Sunday:
+	// 		println("It's the weekend!")
+	// 	default:
+	// 		println("It's a weekday.")
+	// 	}
+
+	// switch with fallthrough
+
+
+	// switch 2 {
+	// 	case 1:
+	// 		println("1")
+	// 		fallthrough
+	// 	case 2:
+	// 		println("2")
+	// 		fallthrough
+	// 	case 3:
+	// 		println("3")
+	// 	default:
+	// 		println("default")
+	// 	}
+
+	
+
+}
